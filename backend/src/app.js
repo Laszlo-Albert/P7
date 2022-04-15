02ada88fa1d2442c5c708691a799463cdf9903e8
Laszlo Albert <albertlacka1@gmail.com>
2022-04-15 16:08:23 +0100

    Installed axios, created signup page skeleton

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,9 +8,9 @@ app.use(morgan('combine'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get ('/status',(req, res) => {
+app.post ('/register', (_req, res) => {
     res.send({
-        message: 'Connection Established'
+        message: 'Your username ${req.body.email} was registered'
     })
 })
 app.listen(process.env.PORT || 8081)
---
+++ frontend/src/components/RegisterUser.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <h1>Register</h1>
+      <input type="email" name="email" v-model="email" placeholder="email">
+      <input type="password" name="password" v-model="password" placeholder="password">
+      <br>
+      <button @click="register">Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+      return {
+        email:'',
+        password:''
+      }  
+  },
+  methods:{
+    register () {
+      console.log('register button was clicked', this.email, this.password)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
