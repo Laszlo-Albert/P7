b2de0710c44a424719af58ba1f1600535a8015c3
Laszlo Albert <albertlacka1@gmail.com>
2022-04-21 16:50:07 +0100

    Set up sequelize, sqlite3 and syntax fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,19 @@ const express = require ('express')
 const bodyParser = require ('body-parser')
 const cors = require ('cors')
 const morgan = require ('morgan')
+const config = require ('./config/config')
+const {sequelize} = require ('./models')
 
 const app = express()
 app.use(morgan('combine'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post ('/register', (req, res) => {
-    res.send({
-        message: `Your username ${req.body.email} was registered`
-    })
+require ('./routes')(app)
+
+// Connects sequelize to the database I have it configured for
+sequelize.sync()
+.then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
 })
-app.listen(process.env.PORT || 8081)
